ef6c55a649adc6a4006774efe600bde6cd1380dc
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-19 19:06:30 +0600

    send varification email

--- src/Pages/Login/RequiredAuth/RequiredAuth.js
+++ src/Pages/Login/RequiredAuth/RequiredAuth.js
@@ -5,6 +5,49 @@ import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
+const RequireAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    if (!user.emailVerified) {
+        return <div className='text-center mt-5'>
+            <h3 className='text-danger'>Your Email is not verified!!</h3>
+            <h5 className='text-success'> Please Verify your email address</h5>
+            <button
+                className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                Send Verification Email Again
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
+    }
+
+    return children;
+};
+
+export default RequireAuth;
+
+
+
+/* import React from 'react';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
+
 const RequiredAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const [sendEmailVerification, sending, error] = useSendEmailVerification(
@@ -35,7 +78,9 @@ const RequiredAuth = ({ children }) => {
     //         <ToastContainer></ToastContainer>
     //     </div>
     // }
+
+  
     return children;
 };
 
-export default RequiredAuth;
+export default RequiredAuth; */
