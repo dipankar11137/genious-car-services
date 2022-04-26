89e34d672e10d13a62f5f229b0b60beed498e312
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-26 21:10:42 +0600

    add server and different things

---
+++ src/Pages/ManageServices/ManageServices.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import useServices from '../../hooks/useServices';
+
+const ManageServices = () => {
+    const [services, setServices] = useServices();
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure ?');
+        if (proceed) {
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+
+                    const remainning = services.filter(service => service._id !== id)
+                    setServices(remainning);
+                    toast('Delete this services');
+                })
+        }
+    }
+
+    return (
+        <div className='w-50 mx-auto'>
+            <h2>Manages Your Services</h2>
+            {
+                services.map(service => <div key={service._id}>
+                    <h5>{service.name} <button
+                        onClick={() => handleDelete(service._id)}
+                        className='btn btn-primary'
+                    >X</button></h5>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ManageServices;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/service/${serviceId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, []);
+
     return (
         <div className='text-center'>
-            <h2>Welcome to details {serviceId}</h2>
+            <h2>You are about the book : </h2>
+            <div>
+                <img src={service.img} alt="" />
+                <h3>Name : {service.name}</h3>
+            </div>
             <Link to="/checkout">
                 <button className='btn btn-primary'>Proceed Checkout</button>
             </Link>
