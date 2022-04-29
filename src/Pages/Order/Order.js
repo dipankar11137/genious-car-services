9d5297b28471026eea2b752c4a82e251d182a3e4
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-29 18:06:23 +0600

    order complate with unauthorize and authorize person

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { async } from '@firebase/util';
+import axios from 'axios';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Order = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            try {
+                const { data } = await axios.get(url, {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                setOrders(data);
+            }
+            catch (error) {
+                console.log(error.message);
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login');
+                }
+            }
+        }
+        getOrders();
+
+        /*  fetch('http://localhost:5000/order')
+             .then(res => res.json())
+             .then(data => setOrders(data)) */
+    }, [user]);
+
     return (
         <div>
-            <h2>Your order</h2>
+            <h2>Your order : {orders.length}</h2>
         </div>
     );
 };
